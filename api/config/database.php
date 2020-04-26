abb17af36ea3c615e1ea9a43d93376cfc5c7c690
Azeez Adeniji <33084662+Ariseaz@users.noreply.github.com>
2020-04-26 12:57:05 +0100

    Code updates for sql connection

--- api/config/database.php
+++ api/config/database.php
@@ -1,23 +1,26 @@
 <?php
 class Database{
  
- 
- /*
     // specify your own database credentials
-    private $host = "localhost";
-    private $db_name = "hospital_db";
-    private $username = "root";
-    private $password = "";
+    private $host = getenv('RDS_HOSTNAME');
+    private $db_name = getenv('RDS_DATABASE');
+    private $username = getenv('RDS_USERNAME');
+    private $password = getenv('RDS_PASSWORD');
     public $conn;
- */
-    $mysqli = new mysqli(getenv('RDS_HOSTNAME'), getenv('RDS_USERNAME'), getenv('RDS_PASSWORD'), getenv('RDS_DATABASE'));
  
     // get the database connection
     public function getConnection(){
  
-        if ($mysqli->connect_errno) {
-      echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-       }
+        $this->conn = null;
+ 
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+ 
+        return $this->conn;
     }
 }
 ?>
